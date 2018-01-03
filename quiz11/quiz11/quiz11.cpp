e72e6fae44dd2df8f4a759f770be92c730165853
Geek song <503455810@qq.com>
2018-01-03 15:20:36 +0800

    2)

--- quiz11/quiz11/quiz11.cpp
+++ quiz11/quiz11/quiz11.cpp
@@ -2,10 +2,67 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+#include <string>
 
+class Fruit
+{
+private:
+	std::string m_name;
+	std::string m_color;
+public:
+	Fruit(std::string name, std::string color) :m_name(name), m_color(color)
+	{
+	}
+
+	std::string getName()
+	{
+		return m_name;
+	}
+
+	std::string getColor()
+	{
+		return m_color;
+	}
+};
+
+class Apple:public Fruit
+{
+public:
+	Apple(std::string color = "red") :Fruit("apple", color)
+	{
+	}
+
+	Apple(std::string name ,std::string color):Fruit(name, color)
+	{
+	}
+};
+
+class GrannySmith:public Apple
+{
+public:
+	GrannySmith(std::string name = "granny smith", std::string color = "green"):Apple(name,color)
+	{
+	}
+};
+
+class Banana:public Fruit
+{
+public:
+	Banana(std::string color = "yellow") :Fruit("banana",color)
+	{
+	}
+};
 
 int main()
 {
-    return 0;
-}
+	Apple a("red");
+	Banana b;
+	GrannySmith c;
 
+	std::cout << "My " << a.getName() << " is " << a.getColor() << ".\n";
+	std::cout << "My " << b.getName() << " is " << b.getColor() << ".\n";
+	std::cout << "My " << c.getName() << " is " << c.getColor() << ".\n";
+
+	return 0;
+}
